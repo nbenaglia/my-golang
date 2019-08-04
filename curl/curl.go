bc1c844def34a771967e1305a08beab32dff325c
Nicola Benaglia <nico.benaz@gmail.com>
2019-08-04 19:11:48 +0200

    Golang interface use and example of curl

---
+++ curl/curl.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// init is called before main.
+func init() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./example2 <url>")
+		os.Exit(-1)
+	}
+}
+
+// main is the entry point for the application.
+func main() {
+	// Get a response from the web server.
+	r, err := http.Get(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Copies from the Body to Stdout.
+	io.Copy(os.Stdout, r.Body)
+	if err := r.Body.Close(); err != nil {
+		fmt.Println(err)
+	}
+}
