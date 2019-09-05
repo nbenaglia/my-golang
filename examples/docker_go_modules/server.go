a1f2c4c9ef64c7db99400d0b2c842e6b110a429b
Nicola Benaglia <nico.benaz@gmail.com>
2019-09-05 14:44:03 +0200

    docker with go modules

---
+++ examples/docker_go_modules/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", rootHandler)
+
+	fmt.Println("Listening on : 9000")
+	http.ListenAndServe(":9000", r)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Docker test server running!")
+
+}
